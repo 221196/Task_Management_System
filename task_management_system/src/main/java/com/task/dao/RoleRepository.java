6535116566f6439ee4404a20f079a5dae64d6139
charmi zala <68703153+221196@users.noreply.github.com>
2021-09-15 00:11:35 +0530

    Delete task_management_system directory

--- task_management_system/src/main/java/com/task/dao/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.task.dao;
-
-import com.task.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByName(String name);
-}
--- task_management_system/src/main/java/com/task/dao/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.task.dao;
-
-import com.task.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository  extends JpaRepository<Task, Integer> {
-    Task findByName(String name);
-}
